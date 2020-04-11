ce73e348a74db54c4d1b0e539985a27e0952acc3
rv40000 <63453334+rv40000@users.noreply.github.com>
2020-04-11 14:46:30 +0300

    картинка

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function makeDonkey()
 {
-  var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Oselslo';
+  var cartos = document.querySelector('img');
+cartos.onclick = function() {
+  var mySrc = cartos.getAttribute('src');
+    if(mySrc === '1522765869310.jpg') {
+      cartos.setAttribute ('src','shit.jpg');
+    } else {
+      cartos.setAttribute ('src','1522765869310.jpg');
 }
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+
 
