00ab5b51160f5227ada9958679932f7bcd4ccdad
rv40000 <63453334+rv40000@users.noreply.github.com>
2020-04-11 14:08:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ function makeDonkey()
   var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Oselslo';
 }
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
