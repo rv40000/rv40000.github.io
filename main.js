d64d7a63ef7eeda1a5a76c8182f4582288f98a9e
rv40000 <63453334+rv40000@users.noreply.github.com>
2020-04-26 20:13:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -104,12 +104,6 @@ function showStuff(jsonObj)
     myDiv.appendChild(myList);
 document.getElementById("tupoiOsel").appendChild(myDiv);
 }
-} else { 
-   var myDiv = document.createElement('div');
-   var dismis = document.createElement('p');
-  dismis.textContent = 'Схерали ты решил, что ' + document.getElementById("req").value + ' тут есть?';
-  myDiv.appendChild(dismis);
-document.getElementById("tupoiOsel").appendChild(myDiv);
 } 
 }
 }
