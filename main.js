caae02fad579702b5c92911b2c8c525baf806bc8
rv40000 <63453334+rv40000@users.noreply.github.com>
2020-04-27 16:05:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -47,8 +47,9 @@ function showStuff(jsonObj)
           document.getElementById("stuly-result").textContent = kychaMebeli[i].название;
           var myList = document.createElement('ul');
           var myDiv = document.createElement('div');
+          var material = kychaMebeli[k].материалы;
           for (var k = 1; k < kychaMebeli.length; k++) 
-          var material = kychaMebeli[i].материалы;
+          
           {
             var Property1 = document.createElement('p');
             var Property2 = document.createElement('p');
