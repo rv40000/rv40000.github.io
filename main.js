ed113f51f9f6e52bf5161decf0c4c63671aca6f7
rv40000 <63453334+rv40000@users.noreply.github.com>
2020-05-03 16:05:25 +0300

    вынес appendChild tupoi-osel из цикла

--- main.js
+++ main.js
@@ -43,13 +43,13 @@ function showStuff(jsonObj)
     let Property2 = document.createElement('p');
     let myDiv = document.createElement('div');
     let myPic = document.getElementById("stulPic");
+    document.getElementById("tupoiOsel").appendChild(myDiv);  
     for (let i = 0; i < kychaMebeli.length; i++)
     {
         if (kychaMebeli[i].название === searchQuery)
         {
             document.getElementById("stuly-result").textContent = kychaMebeli[i].название;
-            document.getElementById("tupoiOsel").appendChild(myDiv);  
-            Property1.textContent = 'производитель: ' + kychaMebeli[i].производитель;
+                        Property1.textContent = 'производитель: ' + kychaMebeli[i].производитель;
             Property2.textContent = 'цена: ' + kychaMebeli[i].цена;
             myDiv.appendChild(Property1);
             myDiv.appendChild(Property2);
