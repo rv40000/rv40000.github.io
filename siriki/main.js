89e08018307b2d6538357f2a23cd4e7649a2a6a8
rv <rv40000@gmail.com>
2020-11-01 18:04:28 +0300

    добавил запрос в мейн

--- siriki/main.js
+++ siriki/main.js
@@ -1,1 +1,26 @@
 ﻿console.log("вачопеке");
+const response = await fetch("/siriki/siriki.json", { method: "GET" })
+    .then(
+        response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка при выполнении запроса: ${response.status}`);
+            }
+            return response.json();
+        }
+    )
+    .then(
+        data => {
+            console.log(data);
+        }
+    )
+    .catch(
+        error => {
+            console.error(error.message);
+        }
+    )
+    .finally(
+        function()
+        {
+            // тут ещё чонить будет
+        }
+    );
